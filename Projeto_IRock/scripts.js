9be6a9fd561b7279b4562ecd113d53d52edfaa7d
lucasadriano97 <lucas.adrianob97@gmail.com>
2022-10-31 22:22:54 -0300

    Irock 1.0

--- Projeto_IRock/scripts.js
+++ Projeto_IRock/scripts.js
@@ -1,5 +1,5 @@
 function touchRock() {
-    var userName = prompt("Qual o seu nome?", "Insira seu nome:");
+    var userName = prompt("Qual o seu nome?", "Insira seu nome");
     if (userName) {
         alert("Prazer em conhecê-lo " + userName + "!");
         document.getElementById("iRock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_happy.png";
