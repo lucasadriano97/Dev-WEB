db5acad656cdcbc1324e738206a1c0a844dd7389
lucasadriano97 <lucas.adrianob97@gmail.com>
2022-10-31 21:58:33 -0300

    Correction in img link

--- Projeto_IRock/scripts.js
+++ Projeto_IRock/scripts.js
@@ -2,7 +2,7 @@ function touchRock() {
     var userName = prompt("Qual o seu nome?", "Insira seu nome:");
     if (userName) {
         alert("Prazer em conhecê-lo " + userName + "!");
-        document.getElementById("iRock").src = "irock_happy.png";
+        document.getElementById("iRock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_happy.png";
     }
 
 }
