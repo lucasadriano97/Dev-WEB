c6fe90c9c7f95abf353e5c4890686c3214680178
lucasadriano97 <lucas.adrianob97@gmail.com>
2022-11-01 22:30:26 -0300

    Click Events Created

---
+++ Teste_Events_JS/scripts.js
@@ -0,0 +1,13 @@
+function touchRock() {
+        document.getElementById("irock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_happy.png";
+    alert("Você deixou o IRock feliz!");
+}
+function touchRocksad() {
+    document.getElementById("irock2").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_sad.png";
+    alert("Você deixou o IRock triste!");
+}
+function touchRockreset() {
+    document.getElementById("irock2").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock.png";
+    document.getElementById("irock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock.png";
+    
+}
