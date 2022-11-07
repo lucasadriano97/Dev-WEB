79c611d09e4ca440d6c41b592c0f8d835192a8aa
Lucas Couto <90484947+lucasadriano97@users.noreply.github.com>
2022-11-07 17:24:23 -0300

    Creating Cookies to save the userName

--- Projeto_IRock/scripts.js
+++ Projeto_IRock/scripts.js
@@ -1,13 +1,62 @@
-function touchRock() {
-    var userName = prompt("Qual o seu nome?", "Insira seu nome");
-    if (userName) {
-        alert("Prazer em conhecê-lo " + userName + "!");
-        document.getElementById("iRock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_happy.png";
-        setTimeout("document.getElementById('iRock').src = 'https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock.png'", 5000);
+function setCookie(name, value, date) {
+    var validade = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        validade = "; expires=" + date.toUTCString();
     }
-
+    document.cookie = name + "=" + (valor || "") + validade + "; path=/";
 }
 
+function getCookie(name) {
+
+    var nameCookie = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var c = cookies[i];
+        while (c.chartAt(0) == '') c = c.substring(l, c.length);
+        if (c.indexOf(nameCookie) == 0) return c.substring(nameCookie.length, c.lenght);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+
+
+
+function Hello() {
+    userName = getCookie("irock_username");
+    // O nome do usuário será lido a partir do cookie e armazenado na variável userName
+    if (userName)
+        alert("Olá " + userName + ", senti sua falta!");
+    else
+        alert("Olá, eu sou o iRock!");
+}
+
+
+function touchRock() {
+    if (userName) {
+        alert("Agradeço pelo carinho " + userName + "!");
+        //o if no ínicio serve para verificar se já temos um userName armazenado
+    } else {
+        userName = prompt("Digite seu nome:");
+        if (userName) {
+
+            alert("Prazer em conhecê-lo " + userName + "!");
+            setCookie("irock_username", userName, 5 * 365);
+            //Assim que for digitado um nome, será armazenado no cookie irock_username e vinculado a variável Username. O período é de 5 anos (5*365)
+        }
+    }
+    document.getElementById("iRock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_happy.png";
+    setTimeout("document.getElementById('iRock').src = 'https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock.png'", 5000);
+}
+
+
+
 function resizeRock() {
-    document.getElementById("iRock").style.height = (document.body.clientHeight - 100) * 0.9;
+    document.getElementById("iRock").style.height = (document.body.clientHeight - 100) * 0.6 + "px";
+    console.log();
 }
--- Teste_Events_JS/scripts.js
+++ Teste_Events_JS/scripts.js
@@ -1,3 +1,39 @@
+function setCookie(name, value, date) {
+    var validade = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        validade = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (valor || "") + validade + "; path=/";
+}
+
+function getCookie(name) {
+
+    var nameCookie = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var c = cookies[i];
+        while (c.chartAt(0) == '') c = c.substring(l, c.length);
+        if (c.indexOf(nameCookie) == 0) return c.substring(nameCookie.length, c.lenght);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+
+function Hello() {
+    userName = getCookie("test_username");
+    if (userName) {
+        alert("Bem-vindo de volta " + userName + "!");
+    } else {
+        alert("Bem-vindo ao meu Menu de Testes em Javascript!");
+    }
+}
+
 function touchRock() {
     document.getElementById("irock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_happy.png";
     alert("Você deixou o IRock feliz!");
@@ -16,26 +52,27 @@ function touchRockreset() {
 
 function ReturnFormIMC() {
     var userName = document.getElementById("nome").value;
+    setCookie("test_username", userName, 5 * 365);
+
     var userBith = document.getElementById("birth").value;
     var userSex = document.getElementsByClassName("sex");
     var userHeight = parseFloat(document.getElementById("height").value) / 100;
     var userWeight = parseFloat(document.getElementById("weight").value);
     var IMC = userWeight / (userHeight * userHeight);
 
-//  Validação dos campos digitados:
-    if (document.getElementById("nome").value == ""){
-    document.getElementById("Resultado").innerHTML ="<p class='error'>Digite seu Nome Completo</p>";}
-    
-    else if (document.getElementById("height").value == "" ||
-    isNaN(document.getElementById("height").value)){
-    document.getElementById("Resultado").innerHTML ="<p class='error'>Verifique o campo Altura</p>";}
+    //  Validação dos campos digitados:
+    if (document.getElementById("nome").value == "") {
+        document.getElementById("Resultado").innerHTML = "<p class='error'>Digite seu Nome Completo</p>";
+    } else if (document.getElementById("height").value == "" ||
+        isNaN(document.getElementById("height").value)) {
+        document.getElementById("Resultado").innerHTML = "<p class='error'>Verifique o campo Altura</p>";
+    } else if (document.getElementById("weight").value == "" ||
+        isNaN(document.getElementById("weight").value)) {
+        document.getElementById("Resultado").innerHTML = "<p class='error'>Verifique o campo Peso.</p>";
+        alert("Em caso de casa decimal, use o PONTO invés de VÍRGULA")
+    }
 
-    else if (document.getElementById("weight").value == "" ||
-    isNaN(document.getElementById("weight").value)){
-    document.getElementById("Resultado").innerHTML ="<p class='error'>Verifique o campo Peso.</p>";
-    alert("Em caso de casa decimal, use o PONTO invés de VÍRGULA")}
-
-// Caso estiver certo, o resultado é exibido:
+    // Caso estiver certo, o resultado é exibido:
     else {
         console.log("Olá " + userName + "! Seu IMC é de aproximadamente " + IMC.toFixed(2));
         document.getElementById("Resultado").innerHTML = "Olá " + userName + "! Seu IMC é de aproximadamente " + IMC.toFixed(2);
