83b7bedb24eee1fc17658c2c25fd5a0b991798a7
lucasadriano97 <lucas.adrianob97@gmail.com>
2022-11-15 10:42:43 -0300

    Creating cene change

--- Decisions_If_Else/scripts.js
+++ Decisions_If_Else/scripts.js
@@ -1,13 +1,16 @@
 var Resposta = false;
-var Materia;
+var Materia = 0;
 var Questao = 1;
+var Ponto =0;
 
 function Hello() {
     alert("Olá, eu sou o iRock!\n\nPara começar, clique em mim");
 }
 
-function touchRock() {
 
+
+function touchRock() {
+    document.getElementById('clique').style.display = 'none';
     userName = prompt("Digite seu nome:");
     if (userName) {
 
@@ -31,46 +34,45 @@ function selacaoMateria() {
 }
 
 
-function resposta(r) {
+function resposta(r){
     Resposta = r
-    console.log(r)
+    console.log(r);
 
-    if (Materia = "Matematica", Questao = 1, r == true) {
-        alert("Parabéns!");
-        console.log(Questao, Materia)
-        document.getElementById("iRock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_happy.png";
-        setTimeout("document.getElementById('iRock').src = 'https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock.png'", 3000);
-        Questao = 2;
-    }
-    if (Materia = "Matematica", Questao = 1, r == false)
-        alert("Tente novamente");
-    document.getElementById("iRock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_sad.png";
-    setTimeout("document.getElementById('iRock').src = 'https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock.png'", 3000)
-    console.log(Questao, Materia)
-}
-
-
+        if (Materia ==1, Questao == 1, r==true){
+            alert("Parabéns!");
+            document.getElementById("iRock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_happy.png";
+            setTimeout("document.getElementById('iRock').src = 'https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock.png'", 4000);
+            Questao ++;
+            Ponto ++;
+            
+        }
+        if (Materia ==1, Questao == 1, r==false){
+            alert("Tente novamente");
+            document.getElementById("iRock").src = "https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock_sad.png";
+            setTimeout("document.getElementById('iRock').src = 'https://raw.githubusercontent.com/lucasadriano97/Dev-WEB/main/Projeto_IRock/Imagem/irock.png'", 4000)
+            Questao --;
+            document.getElementById(ponto).innerText="Pontos = "+Ponto;
+            }
+         console.log(Materia,Questao) 
+        }
 
 function matematica() {
-    Materia = "Matematica";
-    Questao;
-
-    if (Materia == "Matematica", Questao == 1) {
-        document.getElementById('NumeroPergunta').innerText = "Questão " + Questao;
+    Materia = 1;
+    if (Materia == 1, Questao == 1) {
+        document.getElementById('NumeroPergunta').innerText = "Questão 1";
         document.getElementById('Pergunta').innerHTML = "A soma dos quadrados dos catetos de um triângulo é igual ao quadrado de sua hipotenusa.";
-        Questao = 2;
-        //Verdadeiro
+
+        }
+        if (Materia == 1, Questao == 2) {
+            document.getElementById('NumeroPergunta').innerText = "Questão " + Questao;
+            document.getElementById('Pergunta').innerHTML = "O quadrado da hipotenusa é igual a diferença dos quadrados dos catetos.";
+
+        }
     }
-    if (Materia == "Matematica", Questao == 2) {
-        document.getElementById('NumeroPergunta').innerText = "Questão " + Questao;
-        document.getElementById('Pergunta').innerHTML = "O quadrado da hipotenusa é igual a diferença dos quadrados dos catetos.";
-        //Falso
-    }
-    console.log(Questao, Materia)
-}
 
 
-function resizeRock() {
-    document.getElementById("iRock").style.height = (document.body.clientHeight - 100) * 0.4 + "px";
-    console.log();
-}
+    function resizeRock() {
+        document.getElementById("iRock").style.height = (document.body.clientHeight - 100) * 0.4 + "px";
+       
+    }
+
